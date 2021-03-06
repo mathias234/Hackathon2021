2923be34ac25336c0f243d3494ec16584e2fd5c1
Mathias Danielsen <mathiasda98@gmail.com>
2021-03-06 16:57:53 +0100

    You now have to login to add new event and post comments

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,6 @@ class SessionsController < ApplicationController
   end
   def create
     @user = User.find_by(email: params[:email])
-    puts "Found user: #{@user.email}"
     if @user&.authenticate(params[:password])
       session[:user_id] = @user.id
       redirect_to :controller => 'events', :action => 'index' 
